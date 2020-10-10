23083ca2eb751de9ef63c98486a4ec07a9faf7fe
ankit.bhatt <ankit.bhatt@sugarboxnetworks.com>
2020-10-10 17:37:08 +0530

    integrated socket io

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const port = 8080
+const cors = require('cors')
 
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
 // });
-
+app.use(cors())
 io.on('connection', (socket) => {
+  console.log('connected------------------')
 
   socket.on('disconnect', () => {
-    console.log('diconnected')
+    console.log('diconnected-------------')
   });
 
   socket.on('chat message', (msg) => {
-    socket.broadcast.emit('chat message', msg);
+    console.log('message  ', msg)
+    // socket.broadcast.emit('chat message', msg);
+    io.emit('chat message', msg);
   });
 
   // socket.on('chat message', (msg) => {
   //   console.log('message: ' + msg);
-  //   io.emit('chat message', msg);
   // });
 });
 
