615bc9bffd80dd10e23cd142c6cba0ee71ffcf82
ankit.bhatt <ankit.bhatt@sugarboxnetworks.com>
2020-10-13 15:21:07 +0530

    added env var

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-const port = 8080
+const port = process.env.PORT ||8080
 const cors = require('cors')
 
 // app.get('/', (req, res) => {
